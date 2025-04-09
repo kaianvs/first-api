beb60d0220768fe4f2c53973e0b149316690db16
Kaianvs <kaianvs12@gmail.com>
2025-04-09 18:01:11 -0300

    feat: api http

---
+++ src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        id: 1,
+        name:'Kaian Vasconcelos'
+    },
+    {
+        id: 2,
+        name:'Matheus'
+    },
+    {
+        id: 3,
+        name:'Ramon'
+    },
+    {
+        id: 4,
+        name:'Gaby'
+    },
+]
